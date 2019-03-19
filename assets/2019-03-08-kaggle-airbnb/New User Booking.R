62e86ba16b646d4bc59624c4428b5269984f4d95
Yuki <yuki@originlab.com.cn>
2019-03-19 23:55:10 +0800

    //edit draft

---
+++ assets/2019-03-08-kaggle-airbnb/New User Booking.R
@@ -0,0 +1,49 @@
+#train dataset
+data_train_user <- read.csv(file = "/Users/yuki/Yuki/Local/Kaggle/Airbnb-New User Booking/data/train_users_2.csv")
+head(data_train_user)
+dim(data_train_user)
+#test dataset
+data_test_user <- read.csv(file = "/Users/yuki/Yuki/Local/Kaggle/Airbnb-New User Booking/data/test_users.csv")
+head(data_test_user)
+dim(data_test_user)
+
+# Merge train and test dataset
+data_test_user$country_destination <- NaN
+head(data_test_user)
+data_all_user <- rbind(data_train_user, data_test_user)
+dim(data_all_user)
+# Delete id column
+data_all_user$id <- NULL
+# Rearrange columns by column names
+data_all_user <- data_all_user[,order(names(data_all_user))]
+head(data_all_user)
+dim(data_all_user)
+
+#Replace missing value
+data_all_user$gender[data_all_user$gender == "-unknown-"] <- NA
+#Calculate the number of missing data
+data_NA_proportion <- colSums(is.na(data_all_user)) / dim(data_all_user)[1]
+print(data_NA_proportion)
+#Summary age
+summary(data_all_user$age)
+summary(na.omit(data_all_user$age[data_all_user$age > 100]))
+summary(na.omit(data_all_user$age[data_all_user$age < 18]))
+#Replace age>95 && age < 12 by missing value
+data_all_user$age[data_all_user$age > 95] <- NA
+data_all_user$age[data_all_user$age < 13] <- NA
+summary(data_all_user$age)
+
+library(ggplot2)
+#Gender
+count_gender <- table(data_all_user$gender[,drop=T])
+count_gender <- as.data.frame(count_gender)
+p <- ggplot(data=count_gender, aes(x=Var1, y=Freq))
+p +geom_bar(stat = "identity") + xlab("Gender") + ylab("Count")
+#Gender & Country
+count_country <- table(data_all_user$gender[,drop=T], data_all_user$country_destination)
+count_country <- as.data.frame(count_country)
+p <- ggplot(data=count_country, aes(x=Var2, y=Freq))
+p +geom_bar(stat = "identity", aes(fill=Var1), position = "dodge") + xlab("Country") + ylab("Count") + theme_bw()
+#Age
+p <- ggplot(data=data_all_user,aes(x=age))
+p +geom_histogram(aes(y=..density..), binwidth = 2, color="black", fill="white") + geom_density(alpha=.2, fill="#FF6666") 
